31c6a54d661396645d9809664f989e48895f62db
Daniel Guerra <danielguerra.g12@gmail.com>
2022-06-08 21:59:17 -0300

    primeira parte do hmtl ok.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FeedComponent } from './feed/component/feed.component';
+import { FeedComponent } from './feed/components/feed.component';
 
 const routes: Routes = [{ path: '', component: FeedComponent }];
 
